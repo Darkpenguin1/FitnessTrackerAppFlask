90385860f3aabd59b9711fb7312345a4a2c9b282
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-07-08 20:41:25 -0400

    Updated my db schema

--- myApp/models.py
+++ myApp/models.py
@@ -1,11 +1,14 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from myApp import db
+from datetime import datetime
 
 class User(db.Model):
-    _id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
+    _id = db.Column(db.Integer, primary_key=True)           # Do I have to assign all these values to nullable equals false even though my html form doesnt allow them
+    username = db.Column(db.String(100), nullable=False)    # -- to be null when submitted?
     email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    exercises = db.relationship('Exercise', backref='user', lazy=True)
+
 
     def setPassword(self, password):
         self.password_hash = generate_password_hash(password)
@@ -21,4 +24,27 @@ class User(db.Model):
         self.username = username
         self.email = email
         self.setPassword(password)
+    
+
+class Exercise(db.Model):       ## A new model to represent the properties of exercise (what will be logged)
+    _id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)    
+    weight = db.Column(db.Integer, nullable=True)      ## In the future I want the user to be able to input the number of sets and the weight.  
+    date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)     ## I thought about bw exercises and weight should be nullable 
+    user_id = db.Column(db.Integer, db.ForeignKey('user._id'), nullable=False)
+
+    def __init__(self, name, description, user_id, weight=None, date=None):
+        self.name = name
+        self.description = description
+        self.weight = weight
+        if date is None:
+            self.date = datetime.utcnow()
+        else:
+            self.date = date
+        self.user_id = user_id
+             
+
+    def __repr__(self):
+        return f'<Exercise {self.name} Description: {self.description}>'
     
