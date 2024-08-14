c55f29cb7c8d4e239513682219784c2614a2aaf4
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-08-13 22:45:53 -0400

    up

--- config.py
+++ config.py
@@ -6,9 +6,6 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     
     
