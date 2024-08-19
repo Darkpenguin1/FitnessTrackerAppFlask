2c180e4562bfb376a2851e4b74f5f802927bc391
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-08-18 21:48:08 -0400

    Config

--- config.py
+++ config.py
@@ -15,23 +15,19 @@ class ProductionConfig(Config):
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    
     conn_str = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
     if conn_str:
         conn_str_params = dict(pair.split('=') for pair in conn_str.split(' '))
-
         DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
             dbuser=conn_str_params.get('user'),
             dbpass=conn_str_params.get('password'),
             dbhost=conn_str_params.get('host'),
             dbname=conn_str_params.get('dbname')
         )
+        SQLALCHEMY_DATABASE_URI = DATABASE_URI
     else:
         raise ValueError("AZURE_POSTGRESQL_CONNECTIONSTRING environment variable not set")
 
-    SQLALCHEMY_DATABASE_URI = DATABASE_URI
-
-    # Additional production settings
     SESSION_COOKIE_SECURE = True
     REMEMBER_COOKIE_SECURE = True
     SQLALCHEMY_ENGINE_OPTIONS = {
