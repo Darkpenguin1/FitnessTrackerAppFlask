817317817dcbd4bea885fa48bf863c6d63d94a69
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-08-18 22:44:43 -0400

    Config

--- config.py
+++ config.py
@@ -15,19 +15,12 @@ class ProductionConfig(Config):
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-    conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
-    DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-        dbuser=conn_str_params['user'],
-        dbpass=conn_str_params['password'],
-        dbhost=conn_str_params['host'],
-        dbname=conn_str_params['dbname']
-    )
-    SQLALCHEMY_DATABASE_URI = DATABASE_URI
     SESSION_COOKIE_SECURE = True
     REMEMBER_COOKIE_SECURE = True
     SQLALCHEMY_ENGINE_OPTIONS = {
         'pool_size': 20,
         'max_overflow': 0
     }
+    
