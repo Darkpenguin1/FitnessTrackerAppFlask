dc8c82857ece19682f1136482d736d55a51d3d88
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-08-19 18:53:36 -0400

    Maybe?

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+    conn_str = os.getenv['AZURE_POSTGRESQL_CONNECTIONSTRING']
     conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
     DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
@@ -15,7 +15,7 @@ class Config:
         dbpass=conn_str_params['password'],
         dbhost=conn_str_params['host'],
         dbname=conn_str_params['dbname']
-)
+    )
 
     SESSION_COOKIE_SECURE = True
     REMEMBER_COOKIE_SECURE = True
